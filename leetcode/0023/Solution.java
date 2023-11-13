b44b411be6f9e5d3ac830ee40e462bba6de83a09
coconan <coconan8@gmail.com>
2023-11-13 23:26:17 +0800

    leetcode 0023

--- leetcode/0023/Solution.java
+++ leetcode/0023/Solution.java
@@ -74,3 +74,94 @@ class Solution {
         return result;
     }
 }
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+        return doMerge(lists, 0, lists.length-1);
+    }
+
+    public ListNode doMerge(ListNode[] lists, int left, int right) {
+        if (left == right) {
+            return lists[left];
+        }
+
+        int mid = (left + right) / 2;
+        ListNode a = doMerge(lists, left, mid);
+        ListNode b = doMerge(lists, mid+1, right);
+        return mergeTwoList(a, b);
+    }
+
+    public ListNode mergeTwoList(ListNode a, ListNode b) {
+        if (a == null || b == null) {
+            return a == null ? b : a;
+        }
+
+        ListNode result = null;
+        ListNode cur = null;
+        ListNode minNode = null;
+        while (a != null && b != null) {
+            if (a.val < b.val) {
+                minNode = a;
+                a = a.next;
+            } else {
+                minNode = b;
+                b = b.next;
+            }
+            
+            if (result == null) {
+                result = minNode;
+                cur = minNode;
+            } else {
+                cur.next = minNode;
+                cur = minNode;
+            }
+            cur.next = null;
+        }
+
+        if (a != null) {
+            cur.next = a;
+        }
+        if (b != null) {
+            cur.next = b;
+        }
+
+        return result;
+    }
+}
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+        return doMerge(lists, 0, lists.length-1);
+    }
+
+    public ListNode doMerge(ListNode[] lists, int left, int right) {
+        if (left == right) {
+            return lists[left];
+        }
+
+        int mid = (left + right) / 2;
+        ListNode a = doMerge(lists, left, mid);
+        ListNode b = doMerge(lists, mid+1, right);
+        return mergeTwoList(a, b);
+    }
+
+    public ListNode mergeTwoList(ListNode a, ListNode b) {
+        if (a == null || b == null) {
+            return a == null ? b : a;
+        }
+        
+        if (a.val < b.val) {
+            a.next = mergeTwoList(a.next, b);
+            return a;
+        } else {
+            b.next = mergeTwoList(a, b.next);
+            return b;
+        }
+    }
+}
