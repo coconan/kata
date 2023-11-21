8d0e9a71b908acb96915f5ab216ae2ddc1b9a469
coconan <coconan8@gmail.com>
2023-11-22 00:18:05 +0800

    leetcode 1814

--- leetcode/1814/Solution.java
+++ leetcode/1814/Solution.java
@@ -25,3 +25,30 @@ class Solution {
         return r;
     }
 }
+
+class Solution {
+    public int countNicePairs(int[] nums) {
+        int[] revs = new int[nums.length];
+        Map<Integer, Integer> freq = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            revs[i] = rev(nums[i]);
+        }
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int f = freq.get(nums[i]-revs[i]) != null ? freq.get(nums[i]-revs[i]) : 0;
+            count = (count + f) % (1000000000 + 7);
+            freq.put(nums[i]-revs[i], f+1);
+        }
+        return count;
+    }
+
+    public int rev(int n) {
+        int r = 0;
+        while (n > 0) {
+            r = r * 10 + n % 10;
+            n = n / 10;
+        }
+
+        return r;
+    }
+}
